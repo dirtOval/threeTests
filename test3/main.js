ffc53c7e8017dd1385d3d3582c2b8d74cb5e27d5
deirdre <deirdre.loyall@gmail.com>
2023-04-26 14:33:39 -0400

    trippy effect created in test3

--- test3/main.js
+++ test3/main.js
@@ -1,24 +1,70 @@
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth
+const camera = new THREE.PerspectiveCamera(72, window.innerWidth
   / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+const bgGeometry = new THREE.PlaneGeometry( 26, 26);
+const bgMaterial = new THREE.MeshBasicMaterial( { color: 0xA600FF})
+
+const bg = new THREE.Mesh(bgGeometry, bgMaterial);
+scene.add(bg);
+
+const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+material.transparent = true;
+
+let cubeArray = [];
+for (let i = 0; i < 18; i++) {
+  for (let j = 0; j < 18; j++) {
+    const cube = new THREE.Mesh(geometry, material);
+    bg.add(cube);
+    cube.position.x = i - 9;
+    cube.position.y = j - 9;
+    cubeArray.push(cube);
+  }
+}
 
 camera.position.z = 5;
+// camera.position.x = 0
+
+let cubeOpacity = 1;
+let opacitySwitch = false;
+
 
 function animate() {
   requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  bg.rotation.z += 0.01;
+
+  let rotationSwitch = false;
+
+  if (opacitySwitch) {
+    cubeOpacity += 0.01
+  } else {
+    cubeOpacity -= 0.01;
+  }
+  if (cubeOpacity <= 0.75 || cubeOpacity >= 1) {
+    opacitySwitch = !opacitySwitch
+  }
+
+  material.opacity = cubeOpacity;
+
+  for (let cube of cubeArray) {
+    if (rotationSwitch) {
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+    } else {
+      cube.rotation.x -= 0.01;
+      cube.rotation.y -= 0.01;
+    }
+    cube.rotation.z += 0.07
+    rotationSwitch = !rotationSwitch;
+  }
+
   renderer.render(scene, camera);
 }
 animate();
